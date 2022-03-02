2de66d9c7f22fcaeba1075762b9251854d5fb4bd
rodrigo-francalacci <rodrigo.francalacci@gmail.com>
2022-03-02 05:23:23 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import Nav from './Nav'
+import Hero from './Hero'
+import Content from './Content'
+import Cta from './Cta'
+import Footer from './Footer'
+
+
+export default function Home() {
+
+  return (
+  
+    <>
+    <Head> </Head>
+      <Nav/>
+      <Hero/>
+      <Content/>
+      <Cta/>
+      <Footer/>
+      
+    </>
+      
+    
+  
+
+  )
+
+  
+}
